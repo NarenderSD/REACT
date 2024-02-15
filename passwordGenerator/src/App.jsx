7b0644ace2d7269da98a4c28de3c33ba0674de8f
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-15 22:27:22 +0530

    V10 app css

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [length, setLength] = useState(4)
