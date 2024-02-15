174cce3b5601ee9681242cceccb744ec8e016e96
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-15 16:18:45 +0530

    V7 Props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,34 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Narender",
+    age: 21
+  }
+
+  let newARR = [1, 2, 3]
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4
-      rounded-xl'>Tailwind CSS Test</h1>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://media.licdn.com/dms/image/D5603AQF-ZiO9SOxbsQ/profile-displayphoto-shrink_400_400/0/1706329499820?e=1713398400&v=beta&t=6mXu4DO5JhtjCBH8V2HxaUK6QxgjvvV0vHG2nQjKc84" alt="" width="384" height="512" />
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+      rounded-xl mb-4'>Tailwind CSS Test</h1>
+      <Card username="Thakur ki chai" />
+      <Card username="Narender" />
+     
     </>
   )
 }
