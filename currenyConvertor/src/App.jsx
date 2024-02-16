1e0a41caa8f360df357f786735d2ef2a2650be48
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-16 23:23:24 +0530

    App 1

--- currenyConvertor/src/App.jsx
+++ currenyConvertor/src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from 'react'
-
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {                // hooks
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
 
   return (
     <>
