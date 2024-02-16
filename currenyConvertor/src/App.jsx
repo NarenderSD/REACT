a339954015a7935f2b987242e46558f2178b5852
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-16 22:59:44 +0530

    V11 cc hooks

--- currenyConvertor/src/App.jsx
+++ currenyConvertor/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <>
-     
+     <h1 className='text-3xl bg-orange-500'>Currency Convertor</h1>
     </>
   )
 }
