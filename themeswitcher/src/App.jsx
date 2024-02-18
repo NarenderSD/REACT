58d685ca7674ebc5183bb087797d19e558881e66
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-02-18 17:29:08 +0530

    Theme Switcher

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
 
   return (
-    <>
-      <h1>Theme Swicher</h1>
-    </>
-  )
-}
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <ThemeBtn />
+            </div>
 
+            <div className="w-full max-w-sm mx-auto">
+                <Card />
+            </div>
+        </div>
+    </div>
+  </ThemeProvider>
+)
+}
 export default App
